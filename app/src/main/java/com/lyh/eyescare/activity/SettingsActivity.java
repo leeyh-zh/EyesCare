0a3ef802adb57591337dcee7905ed55c42fe9f17
lyh <lionlyh@163.com>
2017-03-24 17:19:15 +0800

    update

--- app/src/main/java/com/lyh/eyescare/activity/SettingsActivity.java
+++ app/src/main/java/com/lyh/eyescare/activity/SettingsActivity.java
@@ -1,8 +1,6 @@
-package com.lyh.eyescare;
+package com.lyh.eyescare.activity;
 
-import android.app.AlarmManager;
 import android.app.PendingIntent;
-import android.app.TimePickerDialog;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
@@ -12,32 +10,38 @@ import android.graphics.Color;
 import android.hardware.Camera;
 import android.os.Bundle;
 import android.os.IBinder;
-import android.os.SystemClock;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.LinearLayout;
 import android.widget.SeekBar;
+import android.widget.Switch;
 import android.widget.TextView;
-import android.widget.TimePicker;
 
+import com.lyh.eyescare.R;
 import com.lyh.eyescare.constant.Status;
 import com.lyh.eyescare.service.BackService;
-
-import java.util.Calendar;
+import com.lyh.eyescare.view.ColorPopupWindow;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 
-import static com.lyh.eyescare.R.id.btn_light;
-import static com.lyh.eyescare.R.id.btn_switch;
-import static com.lyh.eyescare.R.id.btn_time;
+/**
+ * Created by lyh on 2017/3/24.
+ */
 
-public class MainActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity {
 
+    @BindView(R.id.color_config_title)
+    TextView colorConfigTitle;
+    @BindView(R.id.color_config)
+    TextView colorConfig;
+    @BindView(R.id.color_setting)
+    LinearLayout colorSetting;
     private int red;
     private int alpha;
     private int blue;
@@ -46,22 +50,26 @@ public class MainActivity extends AppCompatActivity {
     private BackService curservice;
     private Camera camera;
 
-    private Boolean eyeProtectionMode = false;
-
     @BindView(R.id.tvpercent)
     TextView tvpercent;
     @BindView(R.id.spercent)
     SeekBar spercent;
-    @BindView(btn_switch)
-    Button btnSwitch;
-    @BindView(btn_light)
-    Button btnLight;
-    @BindView(btn_time)
-    Button btnTime;
-    @BindView(R.id.btn_save)
-    Button btnSave;
-    @BindView(R.id.tvcolor)
-    TextView tvcolor;
+    @BindView(R.id.eyes_title)
+    TextView eyesTitle;
+    @BindView(R.id.eyes_switch)
+    Switch eyesSwitch;
+    @BindView(R.id.light_title)
+    TextView lightTitle;
+    @BindView(R.id.light_switch)
+    Switch lightSwitch;
+    @BindView(R.id.timer_title)
+    TextView timerTitle;
+    @BindView(R.id.timer_switch)
+    Switch timerSwitch;
+    @BindView(R.id.save_config_title)
+    TextView saveConfigTitle;
+    @BindView(R.id.save_config_switch)
+    Switch saveConfigSwitch;
     @BindView(R.id.tvr)
     TextView tvr;
     @BindView(R.id.sr)
@@ -74,8 +82,7 @@ public class MainActivity extends AppCompatActivity {
     TextView tvb;
     @BindView(R.id.sb)
     SeekBar sb;
-    @BindView(R.id.scrollView1)
-    LinearLayout scrollView1;
+    private SharedPreferences settings;
 
     private ServiceConnection conn = new ServiceConnection() {
         @Override
@@ -90,31 +97,29 @@ public class MainActivity extends AppCompatActivity {
     };
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_new);
         ButterKnife.bind(this);
-        SharedPreferences settings = getSharedPreferences("settings", MODE_PRIVATE);
+        initData();
+        initView();
+        statusSettings();
+    }
+
+    private void initData() {
+        settings = getSharedPreferences("settings", MODE_PRIVATE);
         alpha = settings.getInt("alpha", 0);
         red = settings.getInt("red", 0);
         green = settings.getInt("green", 0);
         blue = settings.getInt("blue", 0);
         time = settings.getInt("time", 0);
-        init();
     }
 
-    private void init() {
+    private void initView() {
         tvpercent.setText("" + alpha * 100 / 255 + "%");
         tvr.setText("" + red * 100 / 255 + "%");
         tvg.setText("" + green * 100 / 255 + "%");
         tvb.setText("" + blue * 100 / 255 + "%");
-        if (time != 0) {
-            String minuteString = "" + time % 60;
-            if (minuteString.length() < 2) {
-                minuteString = "0" + minuteString;
-                btnTime.setText("关闭时间" + time / 60 + ":" + minuteString);
-            }
-        }
 
         spercent.setProgress(alpha);
         spercent.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
@@ -122,7 +127,6 @@ public class MainActivity extends AppCompatActivity {
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 alpha = progress;
                 tvpercent.setText("" + progress * 100 / 255 + "%");
-                tvcolor.setBackgroundColor(Color.argb(alpha, red, green, blue));
                 if (curservice != null) {
                     curservice.changeColor(Color.argb(alpha, red, green, blue));
                 }
@@ -144,8 +148,6 @@ public class MainActivity extends AppCompatActivity {
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 red = progress;
                 tvr.setText("" + progress * 100 / 255 + "%");
-                tvcolor.setTextColor(Color.argb(alpha, red, green, blue));
-                tvcolor.setBackgroundColor(Color.argb(alpha, red, green, blue));
                 if (curservice != null) {
                     curservice.changeColor(Color.argb(alpha, red, green, blue));
                 }
@@ -166,8 +168,6 @@ public class MainActivity extends AppCompatActivity {
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 green = progress;
                 tvg.setText("" + progress * 100 / 255 + "%");
-                tvcolor.setTextColor(Color.rgb(255 - red, 255 - green, 255 - blue));
-                tvcolor.setBackgroundColor(Color.argb(alpha, red, green, blue));
                 if (curservice != null) {
                     curservice.changeColor(Color.argb(alpha, red, green, blue));
                 }
@@ -188,8 +188,6 @@ public class MainActivity extends AppCompatActivity {
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 blue = progress;
                 tvb.setText("" + progress * 100 / 255 + "%");
-                tvcolor.setTextColor(Color.rgb(255 - red, 255 - green, 255 - blue));
-                tvcolor.setBackgroundColor(Color.argb(alpha, red, green, blue));
                 if (curservice != null) {
                     curservice.changeColor(Color.argb(alpha, red, green, blue));
                 }
@@ -203,99 +201,75 @@ public class MainActivity extends AppCompatActivity {
             public void onStopTrackingTouch(SeekBar seekBar) {
             }
         });
-
-        tvcolor.setBackgroundColor(Color.argb(alpha, red, green, blue));
-
     }
 
+    private void statusSettings() {
 
-    @OnClick({R.id.spercent, btn_switch, btn_light, btn_time, R.id.btn_save, R.id.sr, R.id.sg, R.id.sb})
-    public void onViewClicked(View view) {
-        switch (view.getId()) {
-
-            case btn_switch:
-                if (btnSwitch.getText().equals("开启护眼模式")) {
-                    Intent intent = new Intent(MainActivity.this, BackService.class);
+        eyesSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    Intent intent = new Intent(SettingsActivity.this, BackService.class);
                     intent.putExtra("color", Color.argb(alpha, red, green, blue));
                     startService(intent);
                     intent.putExtra("status", 1);
-                    AlarmManager am = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-                    PendingIntent pendingIntent = PendingIntent.getService(MainActivity.this, 0,
-                            intent, PendingIntent.FLAG_UPDATE_CURRENT);
-                    am.set(AlarmManager.ELAPSED_REALTIME, SystemClock.elapsedRealtime() +
-                            1000 * 60 * time, pendingIntent);
+                    PendingIntent pendingIntent = PendingIntent.getService(SettingsActivity.this,
+                            0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
                     bindService(intent, conn, Context.BIND_AUTO_CREATE);
-                    btnSwitch.setText("关闭护眼模式");
                 } else {
-                    Intent intent = new Intent(MainActivity.this, BackService.class);
-                    PendingIntent pendIntent = PendingIntent.getBroadcast(MainActivity.this, 0, intent,
-                            PendingIntent.FLAG_UPDATE_CURRENT);
-                    AlarmManager am = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-                    am.cancel(pendIntent);
-                    unbindService(conn);
-                    stopService(intent);
-                    curservice = null;
-                    btnSwitch.setText("开启护眼模式");
+                    Intent intent = new Intent(SettingsActivity.this, BackService.class);
+                    PendingIntent pendIntent = PendingIntent.getBroadcast(SettingsActivity.this,
+                            0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+                    if (curservice != null) {
+                        unbindService(conn);
+                        stopService(intent);
+                        curservice = null;
+                    }
                 }
-                break;
-            case btn_light:
-                if (btnLight.getText().equals("开启手电筒")) {
+            }
+        });
+
+        lightSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
                     camera = Camera.open();
                     camera.startPreview();
                     Camera.Parameters parameter = camera.getParameters();
                     parameter.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
                     camera.setParameters(parameter);
                     camera.startPreview();
-                    btnLight.setText("关闭手电筒");
                 } else {
                     Camera.Parameters parameter = camera.getParameters();
                     parameter.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
                     camera.setParameters(parameter);
                     camera.stopPreview();
                     camera.release();
-                    btnLight.setText("开启手电筒");
                 }
-                break;
-            case btn_time:
-                final Calendar c = Calendar.getInstance();
-                TimePickerDialog dlg = new TimePickerDialog(MainActivity.this,
-                        new TimePickerDialog.OnTimeSetListener() {
-                            public void onTimeSet(TimePicker view, int hourOfDay,
-                                                  int minute) {
-                                time = (hourOfDay - c.get(Calendar.HOUR_OF_DAY)) * 60 + minute
-                                        - c.get(Calendar.MINUTE);
-                                String minuteString = "" + minute;
-                                if (minuteString.length() < 2)
-                                    minuteString = "0" + minuteString;
-                                btnTime.setText("关闭时间 " + hourOfDay + ":"
-                                        + minuteString);
-                                if (curservice != null) {
-                                    Intent intent = new Intent(MainActivity.this, BackService.class);
-                                    intent.putExtra("status", 1);
-                                    AlarmManager am = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-                                    PendingIntent pendIntent = PendingIntent.getService(
-                                            MainActivity.this, 0, intent,
-                                            PendingIntent.FLAG_UPDATE_CURRENT);
-                                    am.set(AlarmManager.ELAPSED_REALTIME, SystemClock.elapsedRealtime()
-                                            + 1000 * 60 * time, pendIntent);
-                                }
-                            }
-                        }, c.get(Calendar.HOUR_OF_DAY), c.get(Calendar.MINUTE),
-                        true);
-                dlg.show();
-                break;
-            case R.id.btn_save:
-                SharedPreferences settings = getSharedPreferences("setting",
-                        MODE_PRIVATE);
-                SharedPreferences.Editor edit = settings.edit();
-                edit.putInt("alpha", alpha);
-                edit.putInt("red", red);
-                edit.putInt("green", green);
-                edit.putInt("blue", blue);
-                edit.putInt("time", time);
-                edit.commit();
-                break;
-        }
+            }
+        });
+
+        colorSetting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+//                View popupView = SettingsActivity.this.getLayoutInflater().inflate(
+//                        R.layout.color_popupwindow, null);
+//                PopupWindow window = new PopupWindow(popupView,400,600);
+//                window.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#F8F8F8")));
+//                // TODO: 2016/5/17 设置可以获取焦点
+//                window.setFocusable(true);
+//                // TODO: 2016/5/17 设置可以触摸弹出框以外的区域
+//                window.setOutsideTouchable(true);
+//                // TODO：更新popupwindow的状态
+//                window.update();
+//                // TODO: 2016/5/17 以下拉的方式显示，并且可以设置显示的位置
+//                window.showAsDropDown(colorSetting, 0, 20);
+//                window.show
+                ColorPopupWindow popupwindow = new ColorPopupWindow(SettingsActivity.this);
+                popupwindow.showAtLocation(colorSetting, Gravity.BOTTOM, 0, 0);
+            }
+        });
     }
 
     @Override
@@ -306,11 +280,9 @@ public class MainActivity extends AppCompatActivity {
             if (curservice != null) {
                 unbindService(conn);
             }
-            Intent intentv = new Intent(MainActivity.this, BackService.class);
+            Intent intentv = new Intent(SettingsActivity.this, BackService.class);
             stopService(intentv);
             curservice = null;
-            btnTime.setText("时间到！");
-            btnSwitch.setText("开启护眼模式");
         }
     }
 
@@ -328,7 +300,7 @@ public class MainActivity extends AppCompatActivity {
                 if (curservice != null) {
                     unbindService(conn);
                 }
-                Intent intent = new Intent(MainActivity.this, BackService.class);
+                Intent intent = new Intent(SettingsActivity.this, BackService.class);
                 stopService(intent);
                 curservice = null;
                 finish();
