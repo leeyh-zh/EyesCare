6aff82e91b50daf9eb79feba3ba2ff0465e49649
leeyh <leeyh_zh@163.com>
2017-03-28 23:07:17 +0800

    update

--- app/src/main/java/com/lyh/eyescare/service/EyesCareService.java
+++ app/src/main/java/com/lyh/eyescare/service/EyesCareService.java
@@ -6,7 +6,9 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 import android.view.accessibility.AccessibilityEvent;
+import android.widget.RemoteViews;
 
 import com.lyh.eyescare.ColorManager;
 import com.lyh.eyescare.ColorView;
@@ -21,10 +23,12 @@ public class EyesCareService extends AccessibilityService {
 
     public static final int TYPE_OPEN = 0;
     public static final int TYPE_CLOSE = 1;
-    private Notification notification;
+    private Notification mNotification;
     private NotificationManager mNotificationManager;
-
+    private RemoteViews contentView;
     private ColorView mColorView;
+    private Intent notificationIntent;
+    private PendingIntent contentIntent;
 
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
@@ -39,38 +43,36 @@ public class EyesCareService extends AccessibilityService {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         if (intent != null) {
-            int status = intent.getIntExtra("status",0);
+            int status = intent.getIntExtra("status", 0);
             int color = intent.getIntExtra("color", 0xffffffff);
             if (status == TYPE_OPEN) {
                 ColorManager.addColorView(this);
                 ColorManager.changeColor(color);
-                Notification.Builder builder = new Notification.Builder(getApplication());
-                builder.setContentTitle("护眼模式已开启");
-                builder.setContentText("单击此处返回主界面");
-                builder.setSmallIcon(R.mipmap.ic_launcher);
-                builder.setTicker("护眼模式开启");
-                builder.setContentInfo("补充内容");
-                builder.setAutoCancel(true);
-                builder.setWhen(System.currentTimeMillis());
-                notification = new Notification();
-                mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-                // 定义通知栏展现的内容信息
-                // 定义下拉通知栏时要展现的内容信
-                Intent notificationIntent = new Intent(this, SettingsActivity.class);
-                PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
-                        notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-                builder.setContentIntent(contentIntent);
-                notification.flags |= Notification.FLAG_ONGOING_EVENT;
-                notification.flags |= Notification.FLAG_NO_CLEAR;
-                notification.flags |= Notification.FLAG_SHOW_LIGHTS;
-                notification.defaults = Notification.DEFAULT_LIGHTS;
-                notification = builder.build();
-                mNotificationManager.notify(1, notification);
+                showNotification();
             } else {
                 ColorManager.removeColorView(this);
             }
         }
         return super.onStartCommand(intent, flags, startId);
+    }
+
+    private void showNotification() {
+        mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotification = new Notification();
+        contentView = new RemoteViews(getPackageName(), R.layout.notification);
+        notificationIntent = new Intent(this, SettingsActivity.class);
+        notificationIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        contentIntent = PendingIntent.getActivity(this, 0,
+                notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setContentIntent(contentIntent);
+        builder.setContent(contentView);
+        mNotification = builder.build();
+        //mNotification.flags |= Notification.FLAG_INSISTENT;
+        //mNotification.flags |= Notification.FLAG_NO_CLEAR;
+        mNotification.flags |= Notification.FLAG_ONGOING_EVENT;
+        mNotificationManager.notify(0, mNotification);
     }
 
     @Override
